54d26522af76515731df614cb403fd852b4535e4
Senan Qulamov <qulamovsenan@gmail.com>
2025-03-28 15:33:43 +0400

    Changes on libraries Update v1.0.1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ThemeController;
-
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', [ThemeController::class, 'home'])->name('home');
+
+Route::get('/db-check', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json(['message' => 'Database connection is successful'], 200);
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'Database connection failed: ' . $e->getMessage()], 500);
+    }
+});
+
+
+Route::get('/session-check', function () {
+    $sessionDriver = config('session.driver');
+    return response()->json(['session_driver' => $sessionDriver], 200);
+});
